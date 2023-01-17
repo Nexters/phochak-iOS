d3c5bcfa32482b3f9637c95bc9c5bb195b6a9dff
Hansangjin98 <onetable98@naver.com>
2023-01-18 03:47:57 +0900

    feat: #2 - DIContainer 구현 및 적용
    Service 모듈에 ServiceRegistrant 구현

---
+++ Targets/Service/Sources/ServiceRegistrant.swift
@@ -0,0 +1,23 @@
+//
+//  ServiceRegistrant.swift
+//  Service
+//
+//  Created by 한상진 on 2023/01/18.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import Core
+import Domain
+import Network
+
+import Swinject
+
+public struct ServiceRegistrant: RegistrantType {
+
+  // MARK: Methods
+  public func register(container: Container) {
+  }
+
+  // MARK: Initializer
+  public init() {}
+}
