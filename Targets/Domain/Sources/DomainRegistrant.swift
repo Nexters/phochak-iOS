3a3e1b98c8bf97b5cd8898f36ef5252f2576a6ca
onetable98@naver.com <>
2023-01-18 03:44:44 +0900

    feat: #2 - DIContainer 구현 및 적용
    Domain 모듈에 DomainRegistrant 구현

---
+++ Targets/Domain/Sources/DomainRegistrant.swift
@@ -0,0 +1,21 @@
+//
+//  DomainRegistrant.swift
+//  Domain
+//
+//  Created by 한상진 on 2023/01/18.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import Core
+
+import Swinject
+
+public struct DomainRegistrant: RegistrantType {
+
+  // MARK: Methods
+  public func register(container: Container) {
+  }
+
+  // MARK: Initializer
+  public init() {}
+}
