db7eff15951f592b05941c48d43e908425b31150
Hansangjin98 <onetable98@naver.com>
2023-01-18 03:45:55 +0900

    feat: #2 - DIContainer 구현 및 적용
    Feature 모듈에 FeatureRegistrant 구현

---
+++ Targets/Feature/Sources/FeatureRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  FeatureRegistrant.swift
+//  Feature
+//
+//  Created by 한상진 on 2023/01/18.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import Core
+import Domain
+
+import Swinject
+
+public struct FeatureRegistrant: RegistrantType {
+
+  // MARK: Methods
+  public func register(container: Container) {
+  }
+
+  // MARK: Initializer
+  public init() {}
+}
