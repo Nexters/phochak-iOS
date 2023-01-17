8d42cf477ac0e033c939aa92bb89a11b3c29d4ee
Hansangjin98 <onetable98@naver.com>
2023-01-18 03:47:06 +0900

    feat: #2 - DIContainer 구현 및 적용
    Network 모듈에 NetworkRegistrant 구현

---
+++ Targets/Network/Sources/NetworkRegistrant.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkRegistrant.swift
+//  Network
+//
+//  Created by 한상진 on 2023/01/18.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import Core
+
+import Swinject
+
+public struct NetworkRegistrant: RegistrantType {
+
+  // MARK: Methods
+  public func register(container: Container) {
+  }
+
+  // MARK: Initializer
+  public init() {}
+}
