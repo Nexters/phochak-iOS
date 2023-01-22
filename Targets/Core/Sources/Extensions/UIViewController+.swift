bfc53d5ccbdb9ec981f10aeb4696412c634d7976
Iannealer <duwjdtn@ajou.ac.kr>
2023-01-22 21:25:28 +0900

    feat: #7 - 기본 익스텐션 제공
    ViewController 라이프사이클 관련 reactive 프로퍼티와 CollectionView 주요 메소드 제공

---
+++ Targets/Core/Sources/Extensions/UIViewController+.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+.swift
+//  Core
+//
+//  Created by Ian on 2023/01/22.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: UIViewController {
+
+  // MARK: Properties
+  var viewDidLoad: Observable<Void> {
+    self.methodInvoked(#selector(Base.viewDidLoad))
+      .map { _ in }
+  }
+
+  var viewWillAppear: Observable<Void> {
+    self.methodInvoked(#selector(Base.viewWillAppear(_:)))
+      .map { _ in }
+  }
+
+  var viewDidAppear: Observable<Void> {
+    self.methodInvoked(#selector(Base.viewDidAppear(_:)))
+      .map { _ in }
+  }
+}
