09a30284cfde69d4282e15e3a95bedfccabf11a9
Hansangjin98 <onetable98@naver.com>
2023-02-03 09:36:46 +0900

    feat: #14 - 프로필 설정 View, Reactor 구현
    - 프로필 설정 ViewController 구현
    - 프로필 설정 Reactor 임시 구현
    - UI 관련 Extension 추가 및 수정

---
+++ Targets/Feature/Sources/ProfileSetting/Reactor/ProfileSettingReactor.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileSettingReactor.swift
+//  Feature
+//
+//  Created by 한상진 on 2023/01/25.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import Domain
+
+import ReactorKit
+
+// TODO: 추후 서버 API 배포되면 구현할 예정 
+final class ProfileSettingReactor: Reactor {
+
+  // MARK: - Properties
+  var initialState: State = .init(isEnableComplete: false)
+
+  // MARK: - Initializer
+  init() {
+
+  }
+
+  enum Action {
+    case tapCheckButton(nickName: String)
+  }
+
+  enum Mutation {
+    case checkDuplication(nickName: String)
+  }
+
+  struct State {
+    var isEnableComplete: Bool
+  }
+
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .tapCheckButton(let nickName):
+      return .just(.checkDuplication(nickName: nickName))
+    }
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    var newState: State = state
+
+    switch mutation {
+    case .checkDuplication(let nickName):
+      newState.isEnableComplete = nickName.count <= 10
+    }
+
+    return newState
+  }
+}
