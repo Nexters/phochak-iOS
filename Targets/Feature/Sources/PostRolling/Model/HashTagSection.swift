b434f248166de286fa7cf6d6cf935a4e2ad79682
Iannealer <duwjdtn@ajou.ac.kr>
2023-02-03 23:17:18 +0900

    feat: #17 - HashTagSection DataSource 정의

---
+++ Targets/Feature/Sources/PostRolling/Model/HashTagSection.swift
@@ -0,0 +1,32 @@
+//
+//  HashTagSection.swift
+//  Feature
+//
+//  Created by Ian on 2023/01/30.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import RxDataSources
+
+struct HashTagSection {
+
+  // MARK: Properties
+  let header: String
+  var items: [String]
+}
+
+extension HashTagSection: AnimatableSectionModelType {
+
+  // MARK: Properties
+  typealias Item = String
+
+  var identity: String {
+    return header
+  }
+
+  // MARK: Initializer
+  init(original: HashTagSection, items: [String]) {
+    self = original
+    self.items = items
+  }
+}
