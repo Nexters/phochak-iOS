a43fc2eef2dcb20f7e3811d9897259bf17337593
Iannealer <duwjdtn@ajou.ac.kr>
2023-02-05 22:22:02 +0900

    feat: #17 - PostRollingScene 구현
    - PostRollingViewController 구현
    
    - PostRollingReactor 구현

---
+++ Targets/Feature/Sources/PostRolling/View/PostRollingViewController.swift
@@ -0,0 +1,162 @@
+//
+//  PostRollingViewController.swift
+//  Feature
+//
+//  Created by Ian on 2023/01/30.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+final class PostRollingViewController: BaseViewController<PostRollingReactor> {
+
+  // MARK: Properties
+  private let flowLayout: UICollectionViewFlowLayout = .init()
+  private lazy var collectionView: UICollectionView = .init(
+    frame: .zero,
+    collectionViewLayout: flowLayout
+  )
+  private let exclameButtonTapSubject: PublishSubject<Int> = .init()
+  private let likeButtonTapSubject: PublishSubject<Int> = .init()
+  private let topGradientView: UIView = .init()
+
+  // MARK: Initializer
+  init(reactor: PostRollingReactor) {
+    super.init()
+
+    self.reactor = reactor
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: Override
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    let gradient = CAGradientLayer().then {
+      $0.frame = .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.1)
+      $0.colors = [
+        UIColor.createColor(.monoGray, .w950, alpha: 0.3).cgColor,
+        UIColor.createColor(.monoGray, .w950, alpha: 0.0).cgColor
+      ]
+      $0.locations = [0.0, 1.0]
+    }
+    topGradientView.layer.insertSublayer(gradient, at: 0)
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+
+    collectionView.setContentOffset(
+      .init(
+        x: collectionView.frame.width,
+        y: 0
+      ),
+      animated: false
+    )
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+  }
+
+  override func setupViews() {
+    super.setupViews()
+
+    flowLayout.do {
+      $0.minimumLineSpacing = 0
+      $0.sectionInset = .zero
+      $0.scrollDirection = .horizontal
+      $0.itemSize = .init(
+        width: view.frame.width,
+        height: view.frame.height
+      )
+    }
+
+    collectionView.do {
+      $0.backgroundColor = .clear
+      $0.contentInsetAdjustmentBehavior = .never
+      $0.registerCell(cellType: DetailPostCell.self)
+      $0.isPagingEnabled = true
+      $0.showsVerticalScrollIndicator = false
+      $0.showsHorizontalScrollIndicator = false
+      view.addSubview($0)
+    }
+
+    collectionView.addSubview(topGradientView)
+  }
+
+  override func setupLayoutConstraints() {
+    collectionView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+
+    topGradientView.snp.makeConstraints {
+      $0.top.equalTo(view.snp.top)
+      $0.leading.trailing.equalToSuperview()
+      $0.height.equalTo(view.frame.height * 0.15)
+    }
+  }
+
+  override func bind(reactor: PostRollingReactor) {
+    bindAction(reactor: reactor)
+    bindState(reactor: reactor)
+  }
+}
+
+// MARK: - Private
+private extension PostRollingViewController {
+  func bindAction(reactor: PostRollingReactor) {
+    Observable.just(())
+      .map { PostRollingReactor.Action.load }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+
+    exclameButtonTapSubject
+      .map { PostRollingReactor.Action.exclameVideoPost(postID: $0) }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+
+    likeButtonTapSubject
+      .map { PostRollingReactor.Action.likeVideoPost(postID: $0) }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+  }
+
+  func bindState(reactor: PostRollingReactor) {
+    reactor.state
+      .map { $0.videoPosts }
+      .distinctUntilChanged()
+      .bind(to: collectionView.rx.items(
+        cellIdentifier: "\(DetailPostCell.self)",
+        cellType: DetailPostCell.self)
+      ) { [weak self] _, post, cell in
+        cell.configure(reactor: .init(videoPost: post))
+        if let exclameButtonTapSubject = self?.exclameButtonTapSubject {
+          cell.exclameButtonTapSubject
+            .subscribe(exclameButtonTapSubject)
+            .disposed(by: cell.disposeBag)
+        }
+
+        if let likeButtonTapSubject = self?.likeButtonTapSubject {
+          cell.likeButtonTapSubject
+            .subscribe(likeButtonTapSubject)
+            .disposed(by: cell.disposeBag)
+        }
+      }
+      .disposed(by: disposeBag)
+  }
+}
