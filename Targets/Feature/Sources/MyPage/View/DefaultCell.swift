9396a4a394f2375a7198165d833054f06ef177fc
Iannealer <duwjdtn@ajou.ac.kr>
2023-02-26 17:26:07 +0900

    feat: 기본 반환용 DefaultCell, DefaultHeaderView 추가

---
+++ Targets/Feature/Sources/MyPage/View/DefaultCell.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultCell.swift
+//  PhoChak
+//
+//  Created by Ian on 2023/02/25.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import UIKit
+
+final class DefaultCell: BaseCollectionViewCell {}
---
+++ Targets/Feature/Sources/MyPage/View/DefaultHeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultHeaderView.swift
+//  PhoChak
+//
+//  Created by Ian on 2023/02/25.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import UIKit
+
+final class DefaultHeaderView: UICollectionReusableView {}
