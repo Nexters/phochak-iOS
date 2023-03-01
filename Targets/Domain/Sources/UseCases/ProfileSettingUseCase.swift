c8ec870719baaf8e533a49b29e82882313c244c1
Hansangjin98 <onetable98@naver.com>
2023-03-02 01:45:33 +0900

    feat: #28 - 닉네임 변경 Domain 구현

---
+++ Targets/Domain/Sources/UseCases/ProfileSettingUseCase.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileSettingUseCase.swift
+//  Domain
+//
+//  Created by 한상진 on 2023/03/01.
+//  Copyright © 2023 PhoChak. All rights reserved.
+//
+
+import RxSwift
+
+public enum ProfileSettingResult: Error {
+  case success
+  case error
+}
+
+public protocol ProfileSettingUseCaseType {
+
+  // MARK: Properties
+  var profileSettingResultSubject: PublishSubject<ProfileSettingResult> { get }
+
+  // MARK: Methods
+  func checkNickName(nickName: String) -> Observable<Bool>
+  func changeNickName(nickName: String) -> Observable<Void>
+}
+
+final class ProfileSettingUseCase: ProfileSettingUseCaseType {
+  
+  // MARK: Properties
+  public let profileSettingResultSubject: PublishSubject<ProfileSettingResult> = .init()
+  private let service: ProfileSettingServiceType
+  private let disposeBag: DisposeBag = .init()
+
+  // MARK: Initializer
+  init(service: ProfileSettingServiceType) {
+    self.service = service
+  }
+
+  // MARK: Methods
+  func checkNickName(nickName: String) -> Observable<Bool> {
+    service.checkDuplicationNickName(nickName: nickName).asObservable()
+  }
+
+  func changeNickName(nickName: String) -> Observable<Void> {
+    service.changeNickName(nickName: nickName)
+      .subscribe(with: self, onSuccess: { owner, _ in
+        owner.profileSettingResultSubject.onNext(.success)
+      }, onFailure: { owner, error in
+        owner.profileSettingResultSubject.onNext(.error)
+      })
+      .disposed(by: disposeBag)
+
+    return .empty()
+  }
+}
