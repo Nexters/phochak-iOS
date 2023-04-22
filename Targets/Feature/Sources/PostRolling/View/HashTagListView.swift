419cc2d3f41e8db0c5931828ad25af04d490a5c5
Jeongsu Yeo <duwjdtn1@gmail.com>
2023-04-22 19:43:54 +0900

    feat: #65 - 상세화면에서 버튼 터치를 통한 포착시에도 햅틱 피드백 제공

--- Targets/Feature/Sources/PostRolling/View/HashTagListView.swift
+++ Targets/Feature/Sources/PostRolling/View/HashTagListView.swift
@@ -70,6 +70,11 @@ extension HashTagListView {
       .map { [weak self] _ in self?.videoPostRelay.value?.id ?? 0 }
       .do(onNext: { [weak self] _ in
         let isLiked = !(self?.videoPostRelay.value?.isLiked ?? false)
+
+        if isLiked {
+          FeedbackGenerator.shared.generate(.success)
+        }
+
         self?.likeButton.setImage(isLiked ? .createImage(.heartOn) : .createImage(.heartOff), for: .normal)
       })
   }
